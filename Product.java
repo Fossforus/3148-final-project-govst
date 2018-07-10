3c1779618436b77f6501924264a37f7826105ff0
Fayzulla <40807143+FayzullaNorkulov@users.noreply.github.com>
2018-07-10 14:42:33 -0500

    Product

---
+++ Product.java
@@ -0,0 +1,48 @@
+public class Product {
+	protected String name, description, size;
+	protected double price;
+	protected int quantity;
+	
+public Product(String aName, String aDescription, String aSize, double aPrice, int aQuantity) {
+	name = aName;
+	description = aDescription;
+	size = aSize;
+	price = aPrice;
+	quantity = aQuantity;
+}
+	
+	public String getName () {
+		return name;
+	}
+	public void setName(String aName) {
+		name = aName;
+	}
+	public String getDescription () {
+		return description;
+	}
+		
+	public void setDescription(String aDescription) {
+			description = aDescription;
+	}
+	public String getSize () {
+		return size;
+	}
+	
+	public void setSize(String aSize) {
+		size = aSize;
+	}
+	public double getPrice () {
+		return price;
+	}
+	public void setPrice(double aPrice) {
+		price = aPrice;
+	}
+	
+	public int getQuantity () {
+		return quantity;
+	}
+	public void setQuantity(int aQuantity) {
+		quantity = aQuantity;
+	}
+	
+}
