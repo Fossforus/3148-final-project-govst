94fbdc6d470384674596bab5888a10a8fd4c82e7
mopakunle <41080242+mopakunle@users.noreply.github.com>
2018-07-10 13:37:11 -0500

    product Cart

---
+++ Cart.java
@@ -0,0 +1,19 @@
+public class Cart
+{
+  private boolean transactionSuccessful;
+  String productName,shippingAddress;
+  double price,shippingPrice,tax,finalPrice;
+  int quantity;
+  double creditCardNumber;
+  int expirationMonthandYear,cvv;
+
+
+  public Cart()
+{
+
+  TransactionSuccessful = False;
+}
+
+  
+
+  
