85de6cb6de049a112a8dcd9eaa0f0af27fc51bbe
Austin Lee <41079800+baccad@users.noreply.github.com>
2018-07-20 14:52:11 -0500

    Add files via upload

---
+++ clothing 2.java
@@ -0,0 +1,93 @@
+
+
+import java.util.Scanner;
+
+import java.text.DecimalFormat;
+
+
+
+
+
+public class shoppingDriver{
+
+    //This is the driver class the collobarates all the other class.
+
+    
+
+    public static void main(String[] args){
+
+        Product p2 = new Product(" Men's Cool Ultra-Soft Light Weight Crew-Neck T-Shirt" , "Keep cool and dry with our crew neck tee shirt, can be worn alone or as a layering piece
+		adding depth to your wardrobe. Reach for this crew-neck T-shirt from 32 Degrees for an easygoing off-duty look. \n"  + 
+  10.99, 3," 32 degrees  ","Clothing");
+
+       
+
+        System.out.println("Product Name : " + p1.getName() + "\n");
+
+        System.out.println("Desciption : " + p1.getDescription() + "\n");
+
+        System.out.println("Size : " + p1.getSize() + "\n");
+
+        System.out.println("Price : $" + p1.getPrice() + "\n");
+
+        System.out.println("Quantity : " + p1.getQuantity() + "\n");
+
+        System.out.println("Seller : " + p1.getSellerName());
+
+        System.out.println("Category : " + p1.getCategory());
+
+            
+
+        Scanner Scan = new Scanner(System.in);
+
+        
+
+        System.out.print("Enter your shipping address : ");
+
+        
+
+        String shippingAddress;
+
+        
+
+        shippingAddress = Scan.nextLine();
+
+        
+
+        Cart c1 = new Cart(p1.getName(),shippingAddress, p1.getQuantity(), p1.getPrice());
+
+        
+
+        System.out.println("\n\nName of product in cart : " + c1.getName());
+
+        System.out.println("Shipping Address : " + c1.getShippingAdress());
+
+        System.out.println("=============================================");
+
+        System.out.println("Quantity : " + p1.getQuantity());
+
+        System.out.println("Price : " + p1.getPrice());
+
+        System.out.println("Shipping Cost : $3");
+
+        System.out.println("Tax : 10%");
+
+        System.err.println("---------------------------------------------");
+
+        
+
+        // Round the output to two decimal places
+
+      DecimalFormat fmt = new DecimalFormat("0.##");
+
+        System.err.println("Final amount due : $" + fmt.format(c1.finalPrice()));
+
+        System.err.println("---------------------------------------------");
+
+        
+
+        
+
+    }
+
+}
---
+++ clothng 1.java
@@ -0,0 +1,94 @@
+
+
+import java.util.Scanner;
+
+import java.text.DecimalFormat;
+
+
+
+
+
+public class shoppingDriver{
+
+    //This is the driver class the collobarates all the other class.
+
+    
+
+    public static void main(String[] args){
+
+        Product p2 = new Product(" Men's Lightweight Cotton Logo Pajama Pants" , "A modern take on loungewear,
+		these jogger pants from Polo Ralph Lauren combine comfortable cotton jersey with a relaxed fit and an 
+		allover signature pony print. \n"  + 
+  24.99, 3," Ralph lauren  ","Clothing");
+
+       
+
+        System.out.println("Product Name : " + p1.getName() + "\n");
+
+        System.out.println("Desciption : " + p1.getDescription() + "\n");
+
+        System.out.println("Size : " + p1.getSize() + "\n");
+
+        System.out.println("Price : $" + p1.getPrice() + "\n");
+
+        System.out.println("Quantity : " + p1.getQuantity() + "\n");
+
+        System.out.println("Seller : " + p1.getSellerName());
+
+        System.out.println("Category : " + p1.getCategory());
+
+            
+
+        Scanner Scan = new Scanner(System.in);
+
+        
+
+        System.out.print("Enter your shipping address : ");
+
+        
+
+        String shippingAddress;
+
+        
+
+        shippingAddress = Scan.nextLine();
+
+        
+
+        Cart c1 = new Cart(p1.getName(),shippingAddress, p1.getQuantity(), p1.getPrice());
+
+        
+
+        System.out.println("\n\nName of product in cart : " + c1.getName());
+
+        System.out.println("Shipping Address : " + c1.getShippingAdress());
+
+        System.out.println("=============================================");
+
+        System.out.println("Quantity : " + p1.getQuantity());
+
+        System.out.println("Price : " + p1.getPrice());
+
+        System.out.println("Shipping Cost : $3");
+
+        System.out.println("Tax : 10%");
+
+        System.err.println("---------------------------------------------");
+
+        
+
+        // Round the output to two decimal places
+
+      DecimalFormat fmt = new DecimalFormat("0.##");
+
+        System.err.println("Final amount due : $" + fmt.format(c1.finalPrice()));
+
+        System.err.println("---------------------------------------------");
+
+        
+
+        
+
+    }
+
+}
